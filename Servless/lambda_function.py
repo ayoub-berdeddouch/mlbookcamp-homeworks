b8d84ea6d79d7f1b91d3327bc008ce94183c0440
Ayoub BERDEDDOUCH <ayoub.berd@gmail.com>
2021-12-01 16:03:37 +0100

    Add files via upload

---
+++ Servless/lambda_function.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+import numpy as np
+import tflite_runtime.interpreter as tflite
+#import tensorflow.lite as tflite
+from io import BytesIO
+from urllib import request
+
+from PIL import Image
+
+def download_image(url):
+    with request.urlopen(url) as resp:
+        buffer = resp.read()
+    stream = BytesIO(buffer)
+    img = Image.open(stream)
+    return img
+
+
+def prepare_image(img, target_size):
+    if img.mode != 'RGB':
+        img = img.convert('RGB')
+    img = img.resize(target_size, Image.NEAREST)
+    return img
+
+def process_img(X):
+    # img to Array
+    x = np.array(X,dtype='float32')
+    
+    x = x.reshape((1,) + x.shape)
+    x = x/255
+    
+    return x
+
+
+
+interpreter = tflite.Interpreter(model_path='cats-dogs-v2.tflite')
+interpreter.allocate_tensors()
+
+input_index = interpreter.get_input_details()[0]['index']
+output_index = interpreter.get_output_details()[0]['index']
+
+
+
+#url = 'https://upload.wikimedia.org/wikipedia/commons/9/9a/Pug_600.jpg'
+def predict(url):
+    
+    X  = prepare_image(download_image(url),(150,150))
+    X  = process_img(X)
+
+    interpreter.set_tensor(input_index, X)
+    interpreter.invoke()
+    preds = interpreter.get_tensor(output_index)
+    float_preds = preds[0].tolist()
+    label = "Dog" if preds[0][0] >= 0.5 else "Cat"
+    #print(label)
+    return  float_preds
+
+#print('Prediction is = {}'.format(predict(url)))
+
+def lambda_handler(event, context):
+    url = event['url']
+    result = predict(url)
+    return result
